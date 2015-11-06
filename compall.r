53ad627d69b4df660ec5db3dad9240ed887129cc
rodazuero <rodazuero@gmail.com>
2015-11-06 17:07:52 -0500

    update compallr
    including grid for different levels of theta and bequests

--- compall.r
+++ compall.r
@@ -1,3 +1,7 @@
+#In this file I will try to fix the behavioral model
+#The issue I have is with the aalpha41m and also with the
+#variance of the behavior-> The closer to zero the better fit
+#we get for some reason. In Behavioral4.cpp we checked all the functions
 
 #0. Housekeeping
 #Cleanning everything
@@ -31,19 +35,43 @@ Ph=1
 h=1
 b=2
 ttheta=0.4
-
+Amax=0
 a_lo=-wl/(1+i)+1.0e-2
 a_hi=b+wl*(1-h)-Ph*h-1.0e-2
-F_root_ab(a_lo,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
-F_root_ab(a_hi,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
-seq(a_lo,a_hi, 0.001)
-F_root_ab(-100000,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
-F_root_ab(1000000,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
-F_root_ab(10,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
 
-F_root_ab(-0.8,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
-F_root_ab(0.8,b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta)
+seq(a_lo,a_hi, 0.001)
+
 
 
 Assetsolver(b,wl,wh,Ph,h,eeta,i,ppsi,ttheta,bbeta,5)
+
+Assetsolver(0.03,wl,wh,Ph,h,eeta,i,ppsi,0.59,bbeta,5)
+
+
+
+
+
+bgrid<-seq(1,5,(5/50))
+tthetagrid<-seq(0.1,0.9,(1/50))
+SIZE=length(bgrid)
+mat<-matrix(list(), nrow=length(bgrid), ncol=length(tthetagrid))
+mat0<-matrix(list(), nrow=length(bgrid), ncol=length(tthetagrid))
+hopt<-matrix(list(), nrow=length(bgrid), ncol=length(tthetagrid))
+par=c(bbeta,eeta,ppsi)
+for (bb in  1:length(bgrid)){
+  for (tt in 1:length(tthetagrid)){
+    mat0[bb,tt]=Assetsolver(bgrid[bb],wl,wh,Ph,0,eeta,i,ppsi,tthetagrid[tt],bbeta,0)
+    mat[bb,tt]=Assetsolver(bgrid[bb],wl,wh,Ph,1,eeta,i,ppsi,tthetagrid[tt],bbeta,0)
+    hopt[bb,tt]=Hopt(par,bgrid[bb],tthetagrid[tt],wl,wh,i,Ph,Amax)
+  }
+}
+
+F_root_ab(-0.54234,wl,0,Ph,ppsi,bbeta,0.1,wh,eeta,1,i)
+
+filled.contour(bgrid,tthetagrid,hopt, main="EEDU")
+
+
+algo1=Assetsolver(bgrid[bb],wl,wh,Ph,0,eeta,i,ppsi,tthetagrid[tt],bbeta,0)
+algo2=Assetsolver(bgrid[bb],wl,wh,Ph,1,eeta,i,ppsi,tthetagrid[tt],bbeta,0)
+
 
